d8ed90f09861e43600fdff7dffdd6e469fb980ec
Malinduumeshan <155830246+Malinduumeshan@users.noreply.github.com>
2024-03-19 20:28:11 +0530

    check

--- lib/inputscr.dart
+++ lib/inputscr.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 
 class InputScreen extends StatelessWidget {
   final systolicController = TextEditingController();
-  final diastolicController = TextEditingController();
+  final diastolicController = 
 
   @override
   Widget build(BuildContext context) {
